fa6d5fad3767923ab8119e907545621308c2ad31
Munay-k <gonzalo.alfacaso@gmail.com>
2022-04-25 17:57:03 -0500

    isam structure created

---
+++ isam/isam.h
@@ -0,0 +1,46 @@
+#ifndef ISAM_H
+#define ISAM_H
+
+#include "file_organization.h"
+
+#include <filesystem>
+#include <fstream>
+#include <map>
+#include <string>
+
+template <typename RecordType>
+class IsamSparse : public FileOrganization<RecordType> {
+public:
+  IsamSparse(std::string file, std::string index)
+      : fileName{file}, indexName{index} {}
+  IsamSparse(std::string file);
+  ~IsamSparse();
+
+  void insert(RecordType record) override;
+  bool remove(char *key) override;
+  RecordType search(char *key) override;
+  void readIndex() override;
+  void writeIndex() override;
+
+  void compressFile();
+
+private:
+  std::string fileName;
+  std::string indexName;
+  std::map<int, int> index;
+};
+
+template<typename RecordType>
+IsamSparse<RecordType>::~IsamSparse() {
+  writeIndex();
+}
+
+template<typename RecordType>
+IsamSparse<RecordType>IsamSparse(std::string file) : fileName{file} {
+  fileName = "indices/isam/" + fileName;
+  std::fstream(indexName, std::ios::binary);
+}
+
+
+
+#endif
