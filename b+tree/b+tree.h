5bf3cadbf7066a97aa8d995d7fbebc1e07b75ec8
renatoseb <renatorodriguezllanos@gmail.com>
2022-04-29 18:04:27 -0500

    start btree

--- b+tree/b+tree.h
+++ b+tree/b+tree.h
@@ -2,33 +2,14 @@
 #define BPTREE_H
 
 #include "../file_organization.h"
+#include "node.h"
 #include <string>
 #include <fstream>
 #include <vector>
 
 using namespace std;
 
-const int R = 3, BUCKET_SIZE = 20; // I used random values
-
-template<typename TypeKey>
-struct NodeB
-{
-    TypeKey keys[R];
-    long childrens[R + 1];
-    int size;
-    bool isLeaf;
-};
-
-template<typename RecordType>
-struct Bucket
-{
-    // Number of records of a bucket
-    RecordType records[BUCKET_SIZE];
-    // Current size of a bucket
-    int size;
-    // Address of next bucket
-    long nextBucket;
-};
+const int R = 3, BUCKET_SIZE = 20, BUFFER_SIZE = 4096; // I used random values
 
 template<typename TypeKey, typename RecordType>
 class BPTree: public FileOrganization
@@ -49,7 +30,7 @@ public:
     void insert(RecordType record) override;                                        // TODO
     bool remove(char *key) override;                                                // TODO
     RecordType search(TypeKey key) override;
-    vector<RecordType> rangesearch(TypeKey initialKey, TypeKey endKey) override; 
+    vector<RecordType> searchInRange(TypeKey initialKey, TypeKey endKey) override; 
 
     // Utils
     Bucket<RecordType> readBucket(string file, long address); 
--- test/record.h
+++ test/record.h
@@ -4,34 +4,35 @@
 #include<string>
 #include<vector>
 #include<iostream>
+#include<cstring>
 #include "fast-cpp-csv-parser/csv.h"
 using namespace std;
 
 struct GameData
 {
     int id;
-    string name;
-    string genres;
-    string platforms;
-    string runs;
-    string releaseDate;
+    char name[30];
+    char genres[15];
+    char platforms[100];
+    int runs;
+    char releaseDate[10];
 };
 
 vector<GameData *> readCSV(string file)
 {
     io::CSVReader<6, io::trim_chars<' '>, io::double_quote_escape<',','\"'>> in("../datasets/game_data.csv");
     in.read_header(io::ignore_extra_column, "ID", "Name", "Genres", "Platforms", "Runs", "ReleaseDate");
-    int id; string name; string genres; string platforms; string runs; string releasedate; 
+    int id; string name; string genres; string platforms; int runs; string releasedate; 
     vector<GameData *> dataset;
     while(in.read_row(id, name, genres, platforms, runs, releasedate))
     {
         GameData *data = new GameData();
         data->id = id;
-        data->name = name;
-        data->genres = genres;
-        data->platforms = platforms;
+        strcpy(data->name, name.c_str());
+        strcpy(data->genres, genres.c_str());
+        strcpy(data->platforms, platforms.c_str());
         data->runs = runs;
-        data->releaseDate = releasedate;
+        strcpy(data->releaseDate, releasedate.c_str());
         dataset.push_back(data);
     }
     return dataset;
